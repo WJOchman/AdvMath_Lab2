2c6ff0f61a726925449303cc6150b46bd361acf3
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-11-06 08:56:19 +0000

    Syntax Error

--- Lab2.m
+++ Lab2.m
@@ -4,7 +4,7 @@ prompt = {'Enter start time:','Enter end time:','Enter increment:'};
 dlgtitle = 'Data Input';
 dims = [1 70];
 definput = {'','',''};
-answer = inputdlg(prompt,dlgtitle,dims,definput)
+answer = inputdlg(prompt,dlgtitle,dims,definput);
 
 start_time = str2num(answer{1});
 end_time = str2num(answer{2});
@@ -13,7 +13,7 @@ increment = str2num(answer{3});
 % Task 2
 
 if start_time >= end_time
-    f = msgbox('Start time cannot be greater than end time!');
+    f = msgbox('Start time cannot be greater than end time!', 'Error!');
 elseif increment >= end_time
-    f = msgbox('The increment cannot be larger than the end time!');
+    f = msgbox('The increment cannot be larger than the end time!', 'Error!');
 end
