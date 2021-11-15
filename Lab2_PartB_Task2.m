a6866d53ddea69bc3265f49cbd3fce3e15e53a14
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-11-15 15:34:33 +0000

    Add files via upload

---
+++ Lab2_PartB_Task2.m
@@ -0,0 +1,19 @@
+clc;
+clear; 
+
+% Lab 2 | Part B | Task 2 
+% Continuous Time Envelopes and the Nyquist Frequency Range (NFR)
+
+% Job 1: Create a UI with 5 inputs (fundamental_frequency (Hz), sampling_frequency (Hz), start_time (s), stop_time (s), increment (s)).
+
+
+% Job 2: Trapping mechanics (if and ifelse statements) to make sure the user only enters valid inputs. 
+
+
+% Job 3: Convert the continuous time signal to a discrete time signal using the defined sampling freuqncy. 
+
+
+% Job 4: The program must then automatically calculate NFR. The program must inform the user whether the signal is within the NFR or not and act appropriately.
+
+
+% Job 5: This is insane, needs to be revisited. 
