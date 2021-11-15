0277664f803c94b5d283d4d9d2d00c75176b17a8
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-11-15 15:10:41 +0000

    Lab 2 | Part B | Task 1 - Done

--- Lab2.m
+++ Lab2.m
@@ -36,6 +36,8 @@ end
 % Part B - Task 1 - Proving the conecpet of infinite aliases 
 
 t = start_time : increment : end_time;
+t1 = start_time : increment : end_time;
+t2 = start_time : increment : end_time;
 x_c1 = sin(2*pi*1*t);
 x_c2 = sin(2*pi*6*t);
 figure('units', 'normalized', 'OuterPosition', [0 0 1 1]);
@@ -43,6 +45,7 @@ figure('units', 'normalized', 'OuterPosition', [0 0 1 1]);
 % hold on;
 % plot (t, x_c2, 'LineWidth', 2, 'Color', 'green'); - 6Hz Sinusoid
 x_c3 = x_c1 + x_c2;
+subplot(3, 1,1);
 plot (t, x_c3, 'LineWidth', 2, 'Color', 'blue');
 hold on;
 
@@ -58,6 +61,7 @@ grid on;
 % Converting to a discrete signal
 
 %The correct signal
+%{
 Nyquist_Frequency = 6;      %6
 Nyquist_Rate = Nyquist_Frequency * 2;
 Sampling_Frequency = Nyquist_Rate * 5;
@@ -74,20 +78,58 @@ stem(nTs, x_disc3, 'Linewidth', 2, 'Color', 'red');
 legend('Continuous Time Signal','Discrete Time Signal');
 fig = gcf;
 saveas(gcf, 'figure1.jpeg');
+%}
 
-% Reduce the sampling frequency from 30Hz to 5Hz. - This is where I am now!
+% Reduce the sampling frequency from 30Hz to 5Hz.
+% Sampling Frequency = 6Hz
 
-Nyquist_Frequency = 1;      %6
+Nyquist_Frequency = 6;     
 Nyquist_Rate = Nyquist_Frequency * 2;
-Sampling_Frequency = Nyquist_Rate * 5;
+Sampling_Frequency = 5;
 Sampling_Period = 1 / Sampling_Frequency;
 Period = 1 / Nyquist_Frequency;
 N = Period / Sampling_Period;
-n = 0:1:1.8*N;            %10.75*N
+n = 0:1:10*N;
 nTs = n * Sampling_Period;
 x_disc1 = sin(2*pi*6*nTs);  
 x_disc2 = sin(2*pi*1*nTs);
-x_disc4 = x_disc1 + x_disc2;
-stem(nTs, x_disc4, 'Linewidth', 2, 'Color', 'red');
+x_disc3 = x_disc1 + x_disc2;
+stem(nTs, x_disc3, 'Linewidth', 2, 'Color', 'red');
+
+% Sampling Frequency = 11Hz
+
+subplot(3, 1, 2);
+plot (t1, x_c3, 'LineWidth', 2, 'Color', 'blue');
+hold on;
+Nyquist_Frequency = 11;     
+Nyquist_Rate = Nyquist_Frequency * 2;
+Sampling_Frequency = 5;
+Sampling_Period = 1 / Sampling_Frequency;
+Period = 1 / Nyquist_Frequency;
+N = Period / Sampling_Period;
+n = 0:1:19*N;           
+nTs = n * Sampling_Period;
+x_disc1 = sin(2*pi*6*nTs);  
+x_disc2 = sin(2*pi*1*nTs);
+x_disc3 = x_disc1 + x_disc2;
+stem(nTs, x_disc3, 'Linewidth', 2, 'Color', 'red');
+
+% Sampling Frequency = 5001Hz
+
+subplot(3, 1, 3);
+plot (t2, x_c3, 'LineWidth', 2, 'Color', 'blue');
+hold on
+Nyquist_Frequency = 5001;     
+Nyquist_Rate = Nyquist_Frequency * 2;
+Sampling_Frequency = 5;
+Sampling_Period = 1 / Sampling_Frequency;
+Period = 1 / Nyquist_Frequency;
+N = Period / Sampling_Period;
+n = 0:1:9000*N; 
+nTs = n * Sampling_Period;
+x_disc1 = sin(2*pi*6*nTs);  
+x_disc2 = sin(2*pi*1*nTs);
+x_disc3 = x_disc1 + x_disc2;
+stem(nTs, x_disc3, 'Linewidth', 2, 'Color', 'red');
 
 % Part B - Task 2
